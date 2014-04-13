0d38ba8053fa4d700f3f43bf838ecd3e1295e925
Matt Molo <molo.matt@gmail.com>
2014-04-13 02:47:29 -0500

    fixed name maybe

--- account.php
+++ account.php
@@ -76,7 +76,7 @@ UserRequests.once('value',
 </tr>
 </table>
 
-<input type="button" margin-right="20px" onclick="post('add.php', 'id', '<?php echo $id ?>')" value="Add stuffs!"><input type="button" onclick="post('opendeliveries.php', '<?php echo $id ?>')" value="Volunteer!">
+<input type="button" margin-right="20px" onclick="post('add.php', 'id', '<?php echo $id ?>')" value="Add stuffs!"><input type="button" onclick="post('opendeliveries.php', 'id', '<?php echo $id ?>')" value="Volunteer!">
 
 
 
